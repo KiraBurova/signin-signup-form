d76e274c429ab01082e65f95576265587f5f8a38
Kira Burova <burovakira11@gmail.com>
2023-07-15 18:57:17 +0200

    fixed styles

--- client/src/features/signin/index.tsx
+++ client/src/features/signin/index.tsx
@@ -1,12 +1,12 @@
-import { gql, useMutation } from '@apollo/client';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import Button from '../../components/button';
-import Input from '../../components/input';
-import Link from '../../components/link';
+import { gql, useMutation } from "@apollo/client";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Button from "../../components/button";
+import Input from "../../components/input";
+import Link from "../../components/link";
 
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 type FormValues = {
   username: string;
@@ -34,43 +34,46 @@ const SignIn = () => {
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     signIn({ variables: { user: data } }).then((response) => {
       toast(response.data.signInUser.message, {
-        type: response.data.signInUser.status === 'error' ? 'error' : 'success',
+        type: response.data.signInUser.status === "error" ? "error" : "success",
       });
-      navigate('/home');
+      navigate("/home");
     });
   };
 
   return (
     <div className={styles.signin}>
-      <div className={styles.formText}>
-        <h1>Sign in.</h1>
-        <span>
-          Not a member? <Link link="/">Sign up</Link>
-        </span>
+      <div className={styles.signinInner}>
+        <div className={styles.formText}>
+          <h1>Sign in.</h1>
+          <span>
+            Not a member? <Link link="/">Sign up</Link>
+          </span>
+        </div>
+        <form className={styles.signinForm} onSubmit={handleSubmit(onSubmit)}>
+          <>
+            <Input
+              placeholder="Username"
+              {...register("username", { required: true })}
+            />
+            <div className={styles.error}>
+              {errors.username && "Username is required"}
+            </div>
+            <Input
+              type="password"
+              placeholder="Password"
+              {...register("password", { required: true })}
+            />
+            <div className={styles.error}>
+              {errors.password && "Password is required"}
+            </div>
+            <div className={styles.error}>{error && error.message}</div>
+            <Button disabled={errors && Object.keys(errors).length > 0}>
+              Sign in
+            </Button>
+          </>
+        </form>
       </div>
-      <form className={styles.signinForm} onSubmit={handleSubmit(onSubmit)}>
-        <>
-          <Input
-            placeholder="Username"
-            {...register('username', { required: true })}
-          />
-          <div className={styles.error}>
-            {errors.username && 'Username is required'}
-          </div>
-          <Input
-            type="password"
-            placeholder="Password"
-            {...register('password', { required: true })}
-          />
-          <div className={styles.error}>
-            {errors.password && 'Password is required'}
-          </div>
-          <div className={styles.error}>{error && error.message}</div>
-          <Button disabled={errors && Object.keys(errors).length > 0}>
-            Sign in
-          </Button>
-        </>
-      </form>
+      <div className={styles.image} />
     </div>
   );
 };
--- client/src/features/signup/index.tsx
+++ client/src/features/signup/index.tsx
@@ -1,11 +1,11 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { gql, useMutation } from '@apollo/client';
-import { useNavigate } from 'react-router-dom';
-import Button from '../../components/button';
-import Input from '../../components/input';
+import { SubmitHandler, useForm } from "react-hook-form";
+import { gql, useMutation } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/button";
+import Input from "../../components/input";
 
-import styles from './styles.module.scss';
-import Link from '../../components/link';
+import styles from "./styles.module.scss";
+import Link from "../../components/link";
 
 type FormValues = {
   username: string;
@@ -35,7 +35,7 @@ const SignUp = () => {
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     signUp({ variables: { user: data } });
-    navigate('/home');
+    navigate("/home");
   };
 
   if (loading) {
@@ -44,60 +44,63 @@ const SignUp = () => {
 
   return (
     <div className={styles.signup}>
-      <div className={styles.formText}>
-        <h1>Create new account.</h1>
-        <span>
-          Already a member? <Link link="/signin">Sign in</Link>
-        </span>
+      <div className={styles.signupInner}>
+        <div className={styles.formText}>
+          <h1>Create new account.</h1>
+          <span>
+            Already a member? <Link link="/signin">Sign in</Link>
+          </span>
+        </div>
+        <form className={styles.signupForm} onSubmit={handleSubmit(onSubmit)}>
+          <>
+            <Input
+              placeholder="Username"
+              {...register("username", { required: true })}
+            />
+            <div className={styles.error}>
+              {errors.username && "Username is required"}
+            </div>
+            <Input
+              placeholder="Email"
+              type="email"
+              {...register("email", { required: true })}
+            />
+            <div className={styles.error}>
+              {errors.email && "Email is required"}
+            </div>
+            <Input
+              autocomplete="new-password"
+              placeholder="Password"
+              type="password"
+              {...register("password", { required: true })}
+            />
+            <div className={styles.error}>
+              {errors.password && "Password is required"}
+            </div>
+            <Input
+              autocomplete="new-password"
+              placeholder="Repeat password"
+              type="password"
+              {...register("repeatPassword", {
+                required: true,
+                validate: () =>
+                  getValues("repeatPassword") === getValues("password"),
+              })}
+            />
+            <div className={styles.error}>
+              {errors.repeatPassword && "Should match password"}
+            </div>
+            {/* TODO: acceptande checkbox */}
+            <div className={styles.error}>{error && error.message}</div>
+            <Button
+              disabled={(errors && Object.keys(errors).length > 0) || loading}
+            >
+              Sign up
+            </Button>
+          </>
+        </form>
       </div>
-      <form className={styles.signupForm} onSubmit={handleSubmit(onSubmit)}>
-        <>
-          <Input
-            placeholder="Username"
-            {...register('username', { required: true })}
-          />
-          <div className={styles.error}>
-            {errors.username && 'Username is required'}
-          </div>
-          <Input
-            placeholder="Email"
-            type="email"
-            {...register('email', { required: true })}
-          />
-          <div className={styles.error}>
-            {errors.email && 'Email is required'}
-          </div>
-          <Input
-            autocomplete="new-password"
-            placeholder="Password"
-            type="password"
-            {...register('password', { required: true })}
-          />
-          <div className={styles.error}>
-            {errors.password && 'Password is required'}
-          </div>
-          <Input
-            autocomplete="new-password"
-            placeholder="Repeat password"
-            type="password"
-            {...register('repeatPassword', {
-              required: true,
-              validate: () =>
-                getValues('repeatPassword') === getValues('password'),
-            })}
-          />
-          <div className={styles.error}>
-            {errors.repeatPassword && 'Should match password'}
-          </div>
-          {/* TODO: acceptande checkbox */}
-          <div className={styles.error}>{error && error.message}</div>
-          <Button
-            disabled={(errors && Object.keys(errors).length > 0) || loading}
-          >
-            Sign up
-          </Button>
-        </>
-      </form>
+      <div className={styles.image} />
     </div>
   );
 };
